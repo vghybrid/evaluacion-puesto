2df4fb6188c0fde0afe84b498af995b381708ab4
vghybrid <anderson.sarmiento13@gmail.com>
2022-04-27 13:10:47 -0300

    Ultimos Cambios

--- mysql5.php
+++ mysql5.php
@@ -3,7 +3,17 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Insertar datos en MySQL lo más optimizado posible
+// Modificar los datos en MySQL lo más optimizado posible.
 
+$idnota->
+$notes="SI LA SOL FA MI RE DO";
+$noteSeparator=explode(" ",$notes);
+
+for ($i=0; $i < 100000; $i++) { 
+    if(($i+1) == 100000) {
+        $sql.="('".$noteSeparator[$i]."');";
+        $sql="UPDATE 'nota_musical'.'notas_musicales' SET 'nota'='$notaSepator[$i]' WHERE  'idnota'=$idnota";
+    }
+}
 
 ?>
